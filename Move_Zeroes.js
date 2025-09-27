aa4e5bb29ea2e5f421dc1102e2615153cde76f05
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2025-09-27 05:01:30 +0300

    Update Move_Zeroes.js
    now i used two pointers technique that beats 100%

--- Move_Zeroes.js
+++ Move_Zeroes.js
@@ -20,3 +20,21 @@ var moveZeroes = function(nums) {
 };
 
 // the link : https://leetcode.com/problems/move-zeroes/
+
+// Alternative soluion that beats 100%
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let left = 0;
+    let right = 0;
+    while (right < nums.length) {
+        if (nums[right] !== 0) {
+            [nums[right] , nums[left]] = [nums[left] , nums[right]];
+            left++;
+        }
+        right++;
+    }
+};
