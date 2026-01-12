807c29f1bc1a02c053053995d0366d416285f692
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-01-12 09:09:38 +0200

    beats 100

---
+++ Binary_Search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;  // Target found
+            } else if (nums[mid] < target) {
+                left = mid + 1;  // Search in the right half
+            } else {
+                right = mid - 1;  // Search in the left half
+            }
+        }
+        
+        return -1;
+    }
+};
+
+// the link : https://leetcode.com/problems/binary-search/
