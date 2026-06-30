798fb4e656b944ae6de43ad7bebe7bec352c6f97
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-06-30 04:19:02 +0300

    Create Drinks.cpp

---
+++ Drinks.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    double sum = 0;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> p[i];
+        sum += p[i];
+    }
+
+    double result = sum / n;
+    cout << fixed << setprecision(12) << result << endl;
+
+    return 0;
+}
+
+// the link : https://codeforces.com/problemset/problem/200/B
