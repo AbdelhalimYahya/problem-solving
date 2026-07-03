63a94de99d1e9673362a8d403a564a04d0e01e5e
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-07-03 09:43:07 +0300

    Create Plus_or_minus.cpp

---
+++ Plus_or_minus.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int test;
+    cin >> test;
+    
+    while (test--) {
+        int a , b , c;
+        cin >> a >> b >> c;
+        
+        if (a+b == c) {
+            cout << "+" << endl;
+        } else {
+            cout << "-" << endl;
+        }
+    }
+
+    return 0;
+}
+
+// the link : https://codeforces.com/problemset/problem/1807/A
