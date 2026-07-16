09e3a5aba5885012f223674ed31cfb0e46eb7f89
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-07-17 01:58:11 +0300

    Add program to check sum of three numbers
    Implement a program to check if any two of three numbers sum to the third.

---
+++ Sum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int test;
+	cin >> test;
+	
+	while (test--) {
+	    int a , b , c;
+	    cin >> a >> b >> c;
+	    
+	    if (a+b == c || a+c == b || b+c == a) {
+	        cout << "YES" << endl;
+	    } else {
+	        cout << "NO" << endl;
+	    }
+	}
+
+
+    return 0;
+}
+
+// the link : https://codeforces.com/problemset/problem/1742/A
