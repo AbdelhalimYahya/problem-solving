f6a545a84e0ba73a898bf3928e8b28190215b931
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-08-11 00:15:28 +0300

    Update Magnets.cpp

--- Magnets.cpp
+++ Magnets.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int num;
+    cin >> num;
+    
+    int count = 1;
+    vector<int> mangnets(num);
+    
+    for (int i = 0 ; i < num ; i++) {
+        cin >> mangnets[i];
+        if (i == 0 || mangnets[i] == mangnets[i-1]) {
+            continue;
+        } else {
+            count++;
+        }
+    }
+    
+    cout << count;
+}
+
+// the link : https://codeforces.com/problemset/problem/344/A
