c2da688862d62096ee768a1bfa0e3742ac615641
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-09-18 01:54:27 +0300

    Create Easy_Problem.cpp

---
+++ Easy_Problem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int test;
+	cin >> test;
+	
+	while (test--) {
+	    int a;
+	    cin >> a;
+	    
+	    a--;
+	    
+	    cout << a << endl;
+	}
+}
+
+// the link : https://codeforces.com/contest/2044/problem/A
