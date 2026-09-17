c43e3bb1db70fb8924b4a81b77657691c01b3c8b
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-09-18 01:24:18 +0300

    Create Creating_Words.cpp

---
+++ Creating_Words.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string a, b;
+        cin >> a >> b;
+
+        swap(a[0], b[0]);
+
+        cout << a << ' ' << b << '\n';
+    }
+
+    return 0;
+}
+
+// the link : https://codeforces.com/contest/1985/problem/A
