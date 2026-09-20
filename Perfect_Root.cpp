fc9826c89df3b60a2baffedeef52b7960f1f7677
Abdelhalim Yahya <www.quran0987654321@gmail.com>
2026-09-21 00:31:22 +0300

    Add Perfect_Root.cpp to print numbers for test cases
    Implement a program to print numbers from 1 to x for multiple test cases.

---
+++ Perfect_Root.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int test;
+	cin >> test;
+    
+    while (test--) {
+        int x;
+        cin >> x;
+        
+        for (int i = 1 ; i < x+1 ; i++) {
+            cout << i << " ";
+        }
+    }
+    
+}
+
+// the link : https://codeforces.com/contest/2185/problem/A
